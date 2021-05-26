4f6640aaead1f4aff40f4f2cb32da09f66430438
supun maduranga <83456156+supunmadurangasl@users.noreply.github.com>
2021-05-26 13:30:57 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -42,10 +42,10 @@ def map(pos):
             [InlineKeyboardButton(text = '▶️', callback_data = "help+2")]
         ]
     elif(pos==len(tr.HELP_MSG)-1):
-        url = "https://github.com/DamienSoukara/FSub-Heroku"
+        url = "https://github.com/supunmadurangasl/FSub-Heroku"
         button = [
-            [InlineKeyboardButton(text = '🗣 Support Chat', url="https://t.me/damienhelp")],
-            [InlineKeyboardButton(text = '🤖 Source Code', url=url)],
+            [InlineKeyboardButton(text = '👨‍💻Support Chat👨‍💻', url="https://t.me/damienhelp")],
+            [InlineKeyboardButton(text = '📦Source Code📦', url=url)],
             [InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}")]
         ]
     else:
